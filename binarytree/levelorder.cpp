fc6b152fa56c215ca0dd62e336343d5d7921fd29
falguni8 <89798656+falguni8@users.noreply.github.com>
2022-07-04 20:20:15 +0530

    Create levelorder.cpp

---
+++ binarytree/levelorder.cpp
@@ -0,0 +1,19 @@
+
+vector<int> Solution::solve(TreeNode* A) {
+    vector<int>l;
+    queue<TreeNode*>q;
+    q.push(A);
+    while(!q.empty()){
+        TreeNode *x=q.front();
+        q.pop();
+        l.push_back(x->val);
+        if(x->right){
+            q.push(x->right);
+        }
+        if(x->left){
+            q.push(x->left);
+        }
+    }
+    reverse(l.begin(),l.end());
+    return l;
+}
